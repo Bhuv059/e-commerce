4b173d0d8e5df232c46eb37d3b6f80eb4edbbde6
Bhuvaneswari Babu <bhuv059@gmail.com>
2023-12-16 20:11:10 +0100

    implement e-commerce finctionality

---
+++ src/app/_components/Footer/FooterComponent/test.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+import classes from './index.module.scss'
+const FooterComponent = () => {
+  return (
+    <div>Footer hjghjgjgjh</div>
+  )
+}
+
+export default FooterComponent
+
+---------------
+
+import React from 'react'
+
+
+import classes from './index.module.scss'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
+import { Gutter } from '../../Gutter'
+
+const FooterComponent = () => {
+  const pathname = usePathname()
+  return (
+      <footer className={noHeaderFooterUrls.includes(pathname)? classes.hide : ''}>
+          <Gutter>
+              <ul className={classes.inclusions}></ul>
+          </Gutter>
+      </footer>
+
+    )
+}
+
+export default FooterComponent
